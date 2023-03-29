372667b1571d96677bf43054220a7cfb4736eb32
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-03-29 12:00:59 +0530

    Modified to Add Default Data Into List Of Product Review management

---
+++ ProductReviewManagementUsingLINQ/Management.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagementUsingLINQ
+{
+    public class Management
+    {
+        List<ProductReviewModel> productList = new List<ProductReviewModel>();
+        public void AddProductReview(List<ProductReviewModel> productReviewList)
+        {
+            this.productList = productReviewList;
+            Console.WriteLine("Review added successfully.");
+
+        }
+        public void DisplayProductReviewList(List<ProductReviewModel> productReviewListDisplay)
+        {
+            foreach (ProductReviewModel Product in productReviewListDisplay)
+            {
+                Console.WriteLine(Product.ProductId + " " + Product.UserId + " " + Product.Rating + " " + Product.Review + " " + Product.IsLike);
+            }
+        }
+    }
+}
--- ProductReviewManagementUsingLINQ/Program.cs
+++ ProductReviewManagementUsingLINQ/Program.cs
@@ -6,6 +6,57 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Product Review Management System Using LINQ");
+            List<ProductReviewModel> list = new List<ProductReviewModel>()
+            {
+                new ProductReviewModel() {ProductId=1, UserId=10, Rating=4, Review="Average", IsLike=true},
+                new ProductReviewModel() {ProductId=2, UserId=11, Rating=3, Review="Bad", IsLike=true},
+                new ProductReviewModel() {ProductId=3, UserId=12, Rating=2, Review="Bad", IsLike=true},
+                new ProductReviewModel() {ProductId=4, UserId=13, Rating=5, Review="Average", IsLike=true},
+                new ProductReviewModel() {ProductId=5, UserId=14, Rating=7, Review="Good", IsLike=true},
+                new ProductReviewModel() {ProductId=6, UserId=15, Rating=1, Review="Bad", IsLike=true},
+                new ProductReviewModel() {ProductId=7, UserId=16, Rating=3, Review="Bad", IsLike=true},
+                new ProductReviewModel() {ProductId=8, UserId=17, Rating=6, Review="Good", IsLike=true},
+                new ProductReviewModel() {ProductId=9, UserId=18, Rating=7, Review="Good", IsLike=true},
+                new ProductReviewModel() {ProductId=10, UserId=19, Rating=3, Review="Average", IsLike=true},
+                new ProductReviewModel() {ProductId=11, UserId=20, Rating=5, Review="Average", IsLike=true},
+                new ProductReviewModel() {ProductId=12, UserId=21, Rating=6, Review="Good", IsLike=true},
+                new ProductReviewModel() {ProductId=13, UserId=22, Rating=4, Review="Average", IsLike=true},
+                new ProductReviewModel() {ProductId=14, UserId=12, Rating=2, Review="Bad", IsLike=true},
+                new ProductReviewModel() {ProductId=15, UserId=14, Rating=7, Review="Good", IsLike=true},
+                new ProductReviewModel() {ProductId=16, UserId=10, Rating=8, Review="Good", IsLike=true},
+                new ProductReviewModel() {ProductId=17, UserId=11, Rating=9, Review="Best", IsLike=true},
+                new ProductReviewModel() {ProductId=18, UserId=12, Rating=9, Review="Best", IsLike=true},
+                new ProductReviewModel() {ProductId=19, UserId=13, Rating=6, Review="Good", IsLike=true},
+                new ProductReviewModel() {ProductId=20, UserId=14, Rating=7, Review="Good", IsLike=true},
+                new ProductReviewModel() {ProductId=21, UserId=15, Rating=1, Review="Bad", IsLike=true},
+                new ProductReviewModel() {ProductId=22, UserId=16, Rating=3, Review="bad", IsLike=true},
+                new ProductReviewModel() {ProductId=23, UserId=17, Rating=6, Review="Good", IsLike=true},
+                new ProductReviewModel() {ProductId=24, UserId=18, Rating=7, Review="Good", IsLike=true},
+                new ProductReviewModel() {ProductId=25, UserId=19, Rating=3, Review="Bad", IsLike=true},
+            };
+            Management management = new Management();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter an option");
+                Console.WriteLine("1:Add product review\n2:Display product review list\n3:Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        management.AddProductReview(list);
+                        break;
+                    case 2:
+                        management.DisplayProductReviewList(list);
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Enter right option");
+                        break;
+                }
+            }
         }
     }
 }
