d0e5bd976e288216a636d7f1c3ec45cf7b48d98f
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-03-29 11:48:42 +0530

    Added Welcome To Product Review Management Using LINQ

---
+++ ProductReviewManagementUsingLINQ/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductReviewManagementUsingLINQ
+{
+    internal class Program
+    {
+        //LINQ - It is Language Integrated Query. It is use for grouping, sorting, and ordering data.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Management System Using LINQ");
+        }
+    }
+}
